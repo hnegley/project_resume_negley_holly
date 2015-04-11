c871a73f9f92ce214066ff2a8d03cbfa0329bd33
Holly Negley <holly.negley@icloud.com>
2015-04-10 22:03:22 -0400

    finished adding sticky plugin

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
 $(document).ready(function() {
 
 	$.backstretch("img/yellow_flowers.jpg");
-
-});
-
-
-$(document).ready(function() {
-
-$(window).load(function(){
-      $("#sticker").sticky({ topSpacing: 0, center:true, className:"hey" });
-
-      
-
+	$("#sticker").sticky({ topSpacing: 0, center:true, className:"hey" });
+}); 
 
 
 
