21123a077a0dbabf27f3dcf2d26b98f72e69cd3d
Holly Negley <holly.negley@icloud.com>
2015-04-10 21:30:03 -0400

    trying to put in second plugin

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ $(document).ready(function() {
 	$.backstretch("img/yellow_flowers.jpg");
 
 });
+
+
+$(document).ready(function() {
+
+$(window).load(function(){
+      $("#sticker").sticky({ topSpacing: 0, center:true, className:"hey" });
+
+      
+
+
+
+
